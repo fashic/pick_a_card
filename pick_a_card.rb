3682762733685aa55ba444589ef9e7ab6e321f29
Alex A. Linker <fashic@bk.ru>
2020-10-17 01:53:39 +0500

    Change of display suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suite = %w[Diamonds Hearts Clubs Spades]
+suite = %w[♦ ♥ ♣ ♠]
 
-puts "#{values.sample} of #{suite.sample}"
+puts "#{values.sample}#{suite.sample}"
